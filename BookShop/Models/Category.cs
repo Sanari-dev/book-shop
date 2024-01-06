fec918c8d3a12a4e8a4df3cb76bd6ff5e147c6d6
Sanari <sanari.dev@gmail.com>
2024-01-06 21:58:23 +0700

    Code cleaning

--- BookShop/Models/Category.cs
+++ BookShop/Models/Category.cs
@@ -12,7 +12,7 @@ namespace BookShop.Models
         [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
-        [Range(1,100, ErrorMessage ="Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
